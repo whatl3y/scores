bb8fb04d1e24d4bac588a0eaa2ab50f47ab2ff28
Lance Whatley <lwhatley@pmgnet.local>
2016-01-16 15:10:21 -0500

    config

---
+++ libs/config.js
@@ -0,0 +1,21 @@
+var self = module.exports = {
+	mysql: {
+		creds: {
+			host: process.env.MYSQL_HOST,
+			user: process.env.MYSQL_USER,
+			password: process.env.MYSQL_PW,
+			database: process.env.MYSQL_DB
+		}
+	},
+	squairs: {
+		domain: "squairs.com",
+		postScorePath: process.env.SQUAIRS_POST_SCORE_PATH,
+		unscoredEventsSql: "CALL " + process.env.SQUAIRS_UNSCORED_PROCEDURE + "()"
+		
+	},
+	scores: {
+		path: function(sport) {
+			return __dirname+"\\..\\files\\"+sport+".txt";
+		}
+	}
+};
