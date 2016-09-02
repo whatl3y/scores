adc5c5e5c75e3193cd731d1899f1c900c2f93560
Lance Whatley <whatl3y@gmail.com>
2016-09-02 10:14:14 -0400

    update scores uri

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async.waterfall([
     Sq.connection.query(config.squairs.unscoredEventsSql,function(e,rows) {
       if (e) return callback(e);
       if (!rows || !rows.length) return callback("No unscores events from the Squairs database to grade.");
-      
+
       return callback(null,rows[0]);
     });
   },
@@ -33,7 +33,7 @@ async.waterfall([
     Score.getAllFinalGames(function(_err,data) {
       if (_err) return callback(_err);
       if (!data || !data.length) return callback("No final games to evalutate from the scores source.");
-      
+
       return callback(null,unscoredEvents,data);
     });
   }
@@ -43,43 +43,48 @@ async.waterfall([
       Sq.connection.destroy();
       return console.trace("Error in main:",err);
     }
-    
+
     try {
       var curGames,visitingScore,homeScore,o;
-      
       for (var _i=0;_i<unscoredEvents.length;_i++) {
         if (Sq.notFinalScoreYet(unscoredEvents[_i]["StartDateGMT"] + " UTC")) continue;
-        
+
         curGames = finalGames.filter(function(g) {
           return Object.keys(g)[0] == leagueMap[unscoredEvents[_i]["SportLeague"]];
         })[0][leagueMap[unscoredEvents[_i]["SportLeague"]]];
-        
-        for (var _j=0;_j<curGames.length;_j++) {
-          var visitingAliases = unscoredEvents[_i]["VisitingTeamAliases"];
-          var homeAliases = unscoredEvents[_i]["HomeTeamAliases"];
-          
-          var o = Score.checkTeams({visiting:visitingAliases,home:homeAliases},curGames[_j]);
-          
-          if (typeof o==="object") {            
-            o.templateID = unscoredEvents[_i]["tid"];
-            o.HomeTeamName = unscoredEvents[_i]["HomeTeamName"];
-            o.VisitingTeamName = unscoredEvents[_i]["VisitingTeamName"];
-            
-            Sq.postScore(o,function(e,ret,original) {
-              if (e) {
-                console.trace("Squairs Error: ",e);
-              } else {
-                console.log("Success: Template ID: "+original.templateID+" - "+original.VisitingTeamName+" "+original.visiting+" at " +original.HomeTeamName+" "+original.home);
-              }
-            });
+
+        if (curGames.length) {
+          for (var _j=0;_j<curGames.length;_j++) {
+            var visitingAliases = unscoredEvents[_i]["VisitingTeamAliases"];
+            var homeAliases = unscoredEvents[_i]["HomeTeamAliases"];
+
+            var o = Score.checkTeams({visiting:visitingAliases,home:homeAliases},curGames[_j]);
+
+            if (typeof o==="object") {
+              o.templateID = unscoredEvents[_i]["tid"];
+              o.HomeTeamName = unscoredEvents[_i]["HomeTeamName"];
+              o.VisitingTeamName = unscoredEvents[_i]["VisitingTeamName"];
+
+              Sq.postScore(o,function(e,ret,original) {
+                if (e) {
+                  console.trace("Squairs Error: ",e);
+                } else {
+                  console.log("Success: Template ID: "+original.templateID+" - "+original.VisitingTeamName+" "+original.visiting+" at " +original.HomeTeamName+" "+original.home);
+                }
+              });
+            } else {
+              console.log("o is not an object: " + o)
+            }
           }
+        } else {
+          // console.log("There are no current games to score.");
         }
       }
-      
+
     } catch(_err) {
       console.trace("Error after IO:",_err);
     }
-    
+
     Sq.connection.destroy();
   }
 );
--- libs/Scores.js
+++ libs/Scores.js
@@ -11,26 +11,26 @@ var config = require("./config.js");
 |          get: make HTTP request to endpoint at ESPN for scores
 |          parseFinalGames: takes the response from this.get() and parses out games
 |              that have completed and their scores
-|ASSUMES:  
-|REVISION HISTORY:  
+|ASSUMES:
+|REVISION HISTORY:
 |      *LJW 12/5/2015 - created
 -----------------------------------------------------------------------------------------*/
 function Scores(sport) {
   //sports include: nfl,nba,ncf,ncb,mlb,nhl
-  
+
   this.sport = sport || "nfl";
-  this.endpoint = "espn.go.com";
+  this.endpoint = "www.espn.com";
   this.path = this.setPath(this.sport);
 }
 
 Scores.prototype.getAllFinalGames = function(cb,events) {
   events = events || [];
   var self=this;
-  
+
   this.remainingEvents = this.remainingEvents || ["nfl","nba","ncf","ncb","nhl","mlb"];
   this.sport = this.remainingEvents[0];
   this.setPath(this.sport);
-  
+
   this.get(function(_err,data) {
     if (_err) return cb(_err);
     self.parseFinalGames(data,function(__err,ret) {
@@ -39,12 +39,12 @@ Scores.prototype.getAllFinalGames = function(cb,events) {
       } else {
         var o = {};
         o[self.sport] = ret;
-        
+
         events.push(o);
       }
-      
+
       self.remainingEvents.shift();
-      
+
       if (self.remainingEvents.length) {
         self.getAllFinalGames(cb,events);
       } else {
@@ -61,10 +61,10 @@ Scores.prototype.setPath = function(sport,raw) {
 
 Scores.prototype.parseFinalGames = function(response,callback) {
   var self = this;
-  
+
   this.unserialize(response,function(err,oResponse) {
     if (err) return callback(err);
-    
+
     try {
       var ret = [];
       for (var _key in oResponse) {
@@ -72,22 +72,22 @@ Scores.prototype.parseFinalGames = function(response,callback) {
           if (self.isFinal(oResponse[_key].toLowerCase())) {
             var formatted = oResponse[_key].replace(/([^\w\d\s\(\)'-\.]*)([\w\d\s\(\)'\-&\.]+\s\d{1,3})(\W*)(\s|\^)([\w\d\s\(\)'\-&\.]+\s\d{1,3})(.*)(\(.+\))(.*)/,"$2|$5");
             var teams = formatted.split("|");
-            
+
             var o = {};
             for (var _i=0;_i<teams.length;_i++) {
               var team = teams[_i].replace(/^([\(\d\)\s]*)(.+)(\s)(\d{1,3})$/,"$2");
               var score = teams[_i].replace(/^([\(\d\)\s]*)(.+)(\s)(\d{1,3})$/,"$4");
-              
+
               o[team] = Number(score);
             }
-            
+
             ret.push(o);
           }
         }
       }
-      
+
       return callback(null,ret);
-      
+
     } catch(err) {
       return callback(err);
     }
@@ -101,7 +101,7 @@ Scores.prototype.checkTeams = function(aliases,gameInfo) {
   var team2 = Object.keys(gameInfo)[1];
   var team1score = gameInfo[team1];
   var team2score = gameInfo[team2];
-  
+
   var checkTeam = function(aliases,team) {
     for (var _i=0;_i<aliases.length;_i++) {
       if (aliases[_i] == team) {
@@ -110,30 +110,30 @@ Scores.prototype.checkTeams = function(aliases,gameInfo) {
     }
     return false;
   }
-  
+
   var v1 = checkTeam(visitingAliases,team1);
   var v2 = checkTeam(visitingAliases,team2);
   var h1 = checkTeam(homeAliases,team1);
   var h2 = checkTeam(homeAliases,team2);
-  
+
   if (v1 && h2) {
     return {home:team2score,visiting:team1score};
   } else if (v2 && h1) {
     return {home:team1score,visiting:team2score};
   }
-  
+
   return false;
 }
 
 Scores.prototype.isFinal = function(text) {
   var endOfGameText = ["(final)","(end of 4th)","(00:00 in 4th)","(final - ot)","(final - 2nd ot)","(final - 3rd ot)"];
-  
+
   for (var _i=0;_i<endOfGameText.length;_i++) {
     if (text.search(endOfGameText[_i].toLowerCase()) > -1) {
       return true;
     }
   }
-  
+
   return false;
 }
 
@@ -148,18 +148,18 @@ Scores.prototype.unserialize = function(string,cb) {
       var _a = a[_i].split("=");
       obj[ decodeURIComponent(_a[0] || "") ] = decodeURIComponent(_a[1] || "");
     }
-    
+
     cb(null,obj);
   } catch(err) {
     cb(err);
-  }  
+  }
 }
 
 Scores.prototype.get = function(cb) {
   var self=this;
-  
+
   var filePath = config.scores.path(this.sport);
-  
+
   //check if we have a file that would contain the sport information locally first
   fs.readFile(filePath,{encoding:"utf8"},function(e,fileData) {
     if (e) {        //if no local file, go see if info exists at ESPN endpoint
@@ -170,20 +170,23 @@ Scores.prototype.get = function(cb) {
         port: 80,
         method: "GET",
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'/*,
-          'Content-Length': postData.length*/
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
       };
-      
+
       var req = http.get(options,
         function(res) {
-          var body='';
-          res.on('data',function(chunk) {
-            body += chunk;
-          });
-          res.on('end',function() {
-            cb(null,body);
-          });
+          if (res.statusCode == 200) {
+            var body='';
+            res.on('data',function(chunk) {
+              body += chunk;
+            });
+            res.on('end',function() {
+              cb(null,body);
+            });
+          } else {
+            cb("Bad response; status code: " + res.statusCode + "; headers: " + res.headers)
+          }
         }
       ).on('error', function(e) {
         cb(e);
