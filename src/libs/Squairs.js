bb738317794f833e831c8c768aa5061a4ad44a4a
Lance Whatley <whatl3y@gmail.com>
2017-09-04 10:37:05 -0400

    Use application/json for request

--- src/libs/Squairs.js
+++ src/libs/Squairs.js
@@ -41,14 +41,15 @@ export default class Squairs {
     return new Promise((resolve, reject) => {
       this.request.post({
         url: config.squairs.postScorePath,
-        body: {
+        headers:  {'Content-type': 'application/json'},
+        body: JSON.stringify({
           type: "storeResult",
           template: info.templateID,
           scores: {
             home: info.home,
             visiting: info.visiting
           }
-        }
+        })
       }, (err, httpResponse, body) => {
         if (err) return reject(err)
         if (httpResponse.statusCode >= 400) return reject(body)
